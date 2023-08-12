cc5e8b1c9ffb545523ad06bf357b97bf8edf55b5
gumquat <gumquat@gmail.com>
2023-08-12 12:37:30 -0500

    typos fixed

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
             // when the checkboxes are checked, store the amenity ID in the above dictionary
             amenities[amenityID] = amenityName;
         } else {
-            // when the checkboxes ire unchecked, delete the amenity ID from the dictionary
+            // when the checkboxes are unchecked, delete the amenity ID from the dictionary
             delete amenities[amenityID];
         }
 
-        // updates the h4 tag inside the div "amenities" with the new list of amenities checked
+        // updates the h4 tag inside the div "amenities" with the new list of amenities that are checked
         var amenitiesList = Object.values(amenities).join(', ');
         $('div.Amenities h4').text(amenitiesList);
     });
